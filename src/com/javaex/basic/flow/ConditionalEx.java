f5b6fe5637c92a3d6f5ac183488d6ae67f97c86b
kimyk0101 <kimyk7131@gmail.com>
2024-11-04 14:13:56 +0900

    ifPractice02

--- src/com/javaex/basic/flow/ConditionalEx.java
+++ src/com/javaex/basic/flow/ConditionalEx.java
@@ -11,7 +11,9 @@ public class ConditionalEx {
 //		ifPractice01();
 //		switchEx();
 //		switchEx2();
-		switchPractice();
+//		switchPractice();
+		ifPractice02 ();
+		
 	}
 	private static void ifEx() {
 		Scanner scanner = new Scanner(System.in);
@@ -202,4 +204,32 @@ public class ConditionalEx {
 		}
 		System.out.println(day + "에는 " + action);
 	}
+	
+	
+	private static void ifPractice02 () {
+//		점수 입력 -> 등급 출력
+			//	90점 이상 이면-> A등급
+			//	80점 이상~89점 이면 -> B등급
+			//	70점 이상~79점 이면 -> C등급
+			//	60점 이상~69점 이면 -> D등급
+			//	60점 미만이면 -> F등급
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("점수를 입력하세요 : ");
+		int score = scanner.nextInt();
+		
+		if (score >= 90) {
+			System.out.println("A등급");
+		} else if (score >= 80) {
+			System.out.println("B등급");
+		} else if (score >= 70) {
+			System.out.println("C등급");
+		} else if (score >= 60) {
+			System.out.println("D등급");
+		} else {
+			System.out.println("F등급");
+		}
+		scanner.close();
+	}
+	
+	
 }
