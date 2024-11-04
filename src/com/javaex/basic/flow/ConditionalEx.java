522676df818f9038b86e8bd3e3ba954436de72cd
kimyk0101 <kimyk7131@gmail.com>
2024-11-04 11:22:11 +0900

    if ~ else if ~ else

--- src/com/javaex/basic/flow/ConditionalEx.java
+++ src/com/javaex/basic/flow/ConditionalEx.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 public class ConditionalEx {
 
 	public static void main(String[] args) {
-		ifEx();
-
+//		ifEx();
+//		ifEx2();
+		ifPractice01();
 	}
 	private static void ifEx() {
 		Scanner scanner = new Scanner(System.in);
@@ -26,4 +27,64 @@ public class ConditionalEx {
 		
 		scanner.close();
 	}
+	
+	
+	private static void ifEx2() {
+		
+		Scanner scanner = new Scanner(System.in);
+//		정수 입력 받아서 양수, 0, 음수인지 판별
+		System.out.print("정수를 입력하세요:");
+		int num = scanner.nextInt();
+		
+//		if ~ else if ~ else ~
+		//	일반적인 순차 조건 분기
+		/*
+		if (num == 0) { //	조건 1: 0 체크 -> 나머지: 양수 or 음수
+			System.out.println("0입니다.");
+		} else if (num > 0) { //	조건 2: 양수 체크 -> 나머지: 음수
+			System.out.println("양수입니다.");
+		} else {
+			System.out.println("음수입니다.");
+		}*/
+		
+		//	중첩 if
+		if (num == 0) {
+			System.out.println("0입니다.");
+		} else {
+			//	양수 or 음수
+			if (num > 0) {
+				System.out.println("양수입니다.");
+			} else {
+				System.out.println("음수입니다.");
+			}
+		}
+		scanner.close();
+	}
+	
+	
+	private static void ifPractice01() {
+		//	과목번호 입력 -> 강의실 번호 출력
+		//	1 -> R101호 입니다.
+		//	2 -> R202호 입니다.
+		//	3 -> R303호 입니다.
+		//	4 -> R404호 입니다.
+		//	나머지 -> 상담원에게 문의하세요.
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("과목을 선택하세요\n(1.자바 2.C 3.C++ 4.파이썬)\n과목번호:");
+		int code = scanner.nextInt();
+		
+		if (code == 1) {
+			System.out.println("R101호 입니다.");
+		} else if (code == 2) {
+			System.out.println("R202호 입니다.");
+		} else if (code == 3) {
+			System.out.println("R303호 입니다.");
+		} else if (code == 4) {
+			System.out.println("R404호 입니다.");
+		} else {
+			System.out.println("상담원에게 문의하세요");
+		}
+		scanner.close();
+	}
+	
 }
