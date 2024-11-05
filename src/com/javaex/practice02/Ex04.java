e0bdfd504f16d5a666bf3b37c4e0c078c58b6a48
kimyk0101 <kimyk7131@gmail.com>
2024-11-05 16:03:23 +0900

    Practice02

---
+++ src/com/javaex/practice02/Ex04.java
@@ -0,0 +1,15 @@
+package com.javaex.practice02;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		int x = 0;
+		
+		//	x가 0과 같으면
+		if (x == 0) {
+			System.out.println("x는 0이다.");
+		}
+
+	}
+
+}
