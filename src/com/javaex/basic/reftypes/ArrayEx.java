c965954a5801ad0bb373625ef7d72b8feb79d056
kimyk0101 <kimyk7131@gmail.com>
2024-11-05 16:03:01 +0900

    다차원 배열 연습

--- src/com/javaex/basic/reftypes/ArrayEx.java
+++ src/com/javaex/basic/reftypes/ArrayEx.java
@@ -1,10 +1,14 @@
 //11.05-4
 package com.javaex.basic.reftypes;
 
+import java.util.Arrays;
+
 public class ArrayEx {
 
 	public static void main(String[] args) {
-		usingArray();
+//		usingArray();
+		multiDimArrayEx();
+		
 
 	}
 
@@ -39,5 +43,49 @@ public class ArrayEx {
 		for (int i = 0; i < names.length; ++i) {
 			System.out.printf("%s (%.2f) : score = %d%n", names[i],heights[i],scores[i]);
 		}
+		
+		//	scores 배열이 있음
+		int scores2[] = scores;	//	참조 복제
+		//	참조 복제 -> 객체의 주소를 복사하는 것
+		
+		System.out.println("scores:" + Arrays.toString(scores));
+		System.out.println("scores2:" + Arrays.toString(scores2));
+		scores2[2] = 100;
+		System.out.println("scores2:" + Arrays.toString(scores2));
+		System.out.println("scores:" + Arrays.toString(scores));	//	
 	}
+	
+	
+	private static void multiDimArrayEx() {
+		//	5행 10열의 2차원 배열
+		int [][] twoDimens = new int[5][10];	//	빈 2차원 배열
+		
+		//	기본 데이터가 있을 경우
+		int table [][] = {
+				{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, 
+				{2, 3, 4, 5, 6, 7, 8, 9, 0, 1},
+				{3, 4, 5, 6, 7, 8, 9, 0, 1, 2},
+				{4, 5, 6, 7, 8, 9, 0, 1, 2, 3},
+				{5, 6, 7, 8, 9, 0, 1, 2, 3, 4}
+		};
+		
+		System.out.println("table.length: " + table.length);
+		//	인덱스 범위: table[0] ~ table[table.length - 1]
+		System.out.println("table[0].length: " + table[0].length);
+		//	인덱스 범위: table[0][0] ~ table[0][table[0].length - 1]
+		
+		//	table 배열 내부의 모든 요소의 합을 구하자
+		int sum = 0;
+		for (int row = 0; row < table.length; ++row) {	//	행 루프
+			for (int col = 0; col < table[row].length; ++col) { //	열 루프
+				int val = table[row][col];
+				
+				System.out.print(val + "\t");
+				sum += val;	//	내부 cell 데이터 함산
+			}
+			System.out.println();
+		}
+		System.out.println("합산 결과:" + sum);
+	}
+	
 }
