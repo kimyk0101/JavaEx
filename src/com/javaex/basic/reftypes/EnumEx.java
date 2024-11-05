113450b18f2e6e651ceb42a536b0607f265e1116
kimyk0101 <kimyk7131@gmail.com>
2024-11-05 12:23:12 +0900

    enum 연습

---
+++ src/com/javaex/basic/reftypes/EnumEx.java
@@ -0,0 +1,11 @@
+//11.05-3
+package com.javaex.basic.reftypes;
+
+public class EnumEx {
+
+	public static void main(String[] args) {
+		//	switchPractice()의 enum 버전
+
+	}
+
+}
---
+++ src/com/javaex/basic/reftypes/Week.java
@@ -0,0 +1,12 @@
+//11.05-3 new-enum
+package com.javaex.basic.reftypes;
+
+public enum Week {
+	SUNDAY,
+	MONDAY,
+	TUSEDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY
+}
