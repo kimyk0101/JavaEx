684eb2ad76b2975b40d5a8d28e27bd30bf09d396
kimyk0101 <kimyk7131@gmail.com>
2024-11-06 17:58:13 +0900

    Practice04 7번

--- src/com/javaex/practice04/Ex07.java
+++ src/com/javaex/practice04/Ex07.java
@@ -1,9 +1,16 @@
 package com.javaex.practice04;
 
+import java.util.Scanner;
+
 public class Ex07 {
 
 	public static void main(String[] args) {
 		//	키보드에서 정수로 된 돈의 액수를 입력받아 각 몇 개로 변환되는지 작성하세요. 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("금액: ");
+		
+		int money = scanner.nextInt();
+		int j = 0;
 		int[] wonArray = new int[10];
 		wonArray[0] = 50000;
 		wonArray[1] = 10000;
@@ -15,6 +22,12 @@ public class Ex07 {
 		wonArray[7] = 10;
 		wonArray[8] = 5;
 		wonArray[9] = 1;
+		
+		for (int i = 0; i < wonArray.length; ++i) {
+			j = money / wonArray[i];
+			System.out.println(wonArray[i] + "원: " + j + "개");
+			money -= wonArray[i]*j;
+		}
+		
 	}
-
 }
