2329ba82d8d7fa28b6efb29c82450c4820f67ec5
kimyk0101 <kimyk7131@gmail.com>
2024-11-07 12:24:33 +0900

    생성자

--- src/com/javaex/oop/goods/v2/GoodsApp.java
+++ src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -4,7 +4,8 @@ package com.javaex.oop.goods.v2;
 //	Goods v2. 접근제한자
 class Goods {
 	// 필드 선언
-	//	정보는 은닉하고 Getter와 Setter를 통한 우회 접근이 필요
+	//	정보는 은닉하고 Getter와 Setter를 통한 우회 접근 필요
+	//	데이터를 다루는 (기능) 메서드 함께 구현
 	private String name;
 	private int price;
 	
@@ -24,6 +25,12 @@ class Goods {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	
+	//	일반 메서드
+	public void showInfo() {
+		System.out.println("상품명:" + name);
+		System.out.printf("가격: %,d원%n", price);
+	}
 }
 
 //	Goods 테스트 클래스
@@ -39,17 +46,21 @@ public class GoodsApp {
 		camera.setPrice(400_000);
 		
 //		System.out.printf("%s -> %,d%n", camera.name, camera.price);
-		System.out.printf("%s -> %,d%n", camera.getName(), camera.getPrice());
+//		System.out.printf("%s -> %,d%n", camera.getName(), camera.getPrice());
+		
+		camera.showInfo();
 			
 		Goods desktop = new Goods();
 		desktop.setName("LG그램");
 		desktop.setPrice(900_000);
-		System.out.printf("%s -> %,d%n", desktop.getName(), desktop.getPrice());
+//		System.out.printf("%s -> %,d%n", desktop.getName(), desktop.getPrice());
+		desktop.showInfo();
 		
 		Goods cup = new Goods();
 		cup.setName("머그컵");
 		cup.setPrice(2_000);
-		System.out.printf("%s -> %,d%n", cup.getName(), cup.getPrice());		
+//		System.out.printf("%s -> %,%n", cup.getName(), cup.getPrice());
+		cup.showInfo();
 	}
 
 }
---
+++ src/com/javaex/oop/goods/v3/GoodsApp.java
@@ -0,0 +1,67 @@
+//11.07-4
+package com.javaex.oop.goods.v3;
+
+//	Goods v3. 생성자
+//	생성자가 만들어져 있지 않으면 JVM이 기본생성자를 끼워 넣는다
+//	개발자가 생성자를 만들면 JVM은 기본생성자를 끼워넣지 않는다
+//	Setter가 없으면 Read-Only
+class Goods {
+	//	필드
+	private String name;
+	private int price;
+	
+	//	생성자
+	public Goods(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	//	Getters / Setters
+	public String getName() {	//	Getters
+		return name;
+	}
+	
+//	public void setName(String name) {	//	Setters
+//		this.name = name;	//	this -> 현재 인스턴스 자체
+//	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+//	public void setPrice(int price) {
+//		this.price = price;
+//	}
+	
+	//	일반 메서드
+	public void showInfo() {
+		System.out.println("상품명:" + name);
+		System.out.printf("가격: %,d원%n", price);
+	}
+}
+
+//	Goods 테스트 클래스
+public class GoodsApp {
+
+	public static void main(String[] args) {
+//		Goods camera = new Goods();	
+//		camera.setName("Nikon");
+//		camera.setPrice(400_000);
+		Goods camera = new Goods("Nikon", 400_000);
+		camera.showInfo();
+			
+//		Goods desktop = new Goods();
+//		desktop.setName("LG그램");
+//		desktop.setPrice(900_000);
+		Goods desktop = new Goods("LG그램", 900_000);
+//		desktop.setPrice(1_000);
+		desktop.showInfo();
+		
+//		Goods cup = new Goods();
+//		cup.setName("머그컵");
+//		cup.setPrice(2_000);
+		Goods cup = new Goods("머그컵", 2_000);
+		cup.showInfo();
+	}
+
+}
