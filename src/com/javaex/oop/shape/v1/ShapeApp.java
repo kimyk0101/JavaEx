5bbe0cd9ba1017e90496e78ae56b35f9e070f18f
kimyk0101 <kimyk7131@gmail.com>
2024-11-08 16:09:13 +0900

    추상클래스

---
+++ src/com/javaex/oop/shape/v1/ShapeApp.java
@@ -0,0 +1,16 @@
+package com.javaex.oop.shape.v1;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+		//	추상클래스는 실체화 불가, 부모로서만 존재
+//		Shape s = new Shape();	
+		
+		Rectangle r = new Rectangle(10, 20, 100, 50);
+		r.draw();
+		
+		Circle c = new Circle(10, 10, 30);
+		c.draw();
+	}
+
+}
