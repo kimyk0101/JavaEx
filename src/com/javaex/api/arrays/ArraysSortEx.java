50f4fb0c1040e1a1bce63d3dba3e7c350916c779
kimyk0101 <kimyk7131@gmail.com>
2024-11-11 16:54:46 +0900

    Arrays 정렬

---
+++ src/com/javaex/api/arrays/ArraysSortEx.java
@@ -0,0 +1,74 @@
+//11.11-9
+package com.javaex.api.arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+//	두 객체의 선후관계 비교를 위해 Compareable 인터페이스를 구현
+class Member implements Comparable {
+	//	필드
+	String name;
+	
+	//	생성자
+	public Member(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (o instanceof Member) {
+			Member other = (Member)o;	//	다운캐스팅
+			//	리턴값이 0 -> 순서가 같다
+			//	리턴값이 -1 -> 순서가 앞선다
+			//	리턴값이 1 -> 순서가 뒤다
+			return name.compareTo(other.name);
+		}
+		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		return "Member [name=" + name + "]";
+	}
+}
+
+public class ArraysSortEx {
+
+	public static void main(String[] args) {
+//		basicSortTest();	//	기본 타입 오름차순 정렬
+//		basicSortDescTest();	//	기본 타입 내림차순 정렬
+		sortCustomClassTest();	//	사용자 저의 클래스 정렬
+	}
+	
+	private static void basicSortTest() {
+		int[] scores = { 80, 50, 30, 90, 75, 88, 77 };
+		System.out.println("원본 배열:" + Arrays.toString(scores));
+		
+		Arrays.sort(scores);	//	소트의 기본은 오름차순
+		System.out.println("오름차순 정렬:" + Arrays.toString(scores));	
+	}
+	
+	private static void basicSortDescTest() {
+		//	compareTo가 데이터의 선후 관계를 결정해주므로 기본 데이터타입은 역순 정렬을 할 수 없다
+		//	따라서 기본데이터 타입을 객체 데이터타입으로 바꾸어 주어야 한다
+		Integer[] scores = { 80, 50, 30, 90, 75, 88, 77 };	//	객체 포장에 집어넣음
+		System.out.println("원본 배열:" + Arrays.toString(scores));
+		Arrays.sort(scores, Collections.reverseOrder());	//	import 하기
+		System.out.println("내림차순 정렬:" + Arrays.toString(scores));
+	}
+	
+	private static void sortCustomClassTest() {
+		 //	사용자 정의 클래스의 소팅
+		Member m1 = new Member("홍길동");
+		Member m2 = new Member("고길동");
+		Member m3 = new Member("장길산");
+		Member m4 = new Member("임꺽정");
+		
+		Member[] members = {m1, m2, m3, m4};
+		System.out.println("원본배열:" + Arrays.toString(members));
+		Arrays.sort(members);
+		System.out.println("오름차순 배열:" + Arrays.toString(members));	//	어센딩
+		Arrays.sort(members, Collections.reverseOrder());
+		System.out.println("내림차순 정렬:" + Arrays.toString(members));	//	디센딩
+	}
+}
--- src/com/javaex/api/stringclass/StringEx.java
+++ src/com/javaex/api/stringclass/StringEx.java
@@ -1,10 +1,14 @@
-//11.11-6
+//11.11-6,7
 package com.javaex.api.stringclass;
+
+import java.util.Arrays;
 
 public class StringEx {
 
 	public static void main(String[] args) {
-		stringBasic();
+//		stringBasic();
+//		usefulMethods();
+		stringBufferEx();
 
 	}
 
@@ -28,5 +32,83 @@ public class StringEx {
 		float PI = 3.14159f;
 		String s5 = String.valueOf(PI);	//	실수 데이터를 문자열로 변환
 		System.out.println(s5);
+	}
+	
+//	11.11-7
+	private static void usefulMethods() {
+		//	유용한 메서드들
+		String str = "Java Programming, JavaScript Programming";
+		
+		//	문자열의 길이
+		System.out.println(str.length());
+		
+		//	변환 메서드
+		System.out.println(str.toUpperCase());	//	전부 대문자로 바꾸는 메소드
+		System.out.println(str.toLowerCase());	//	전부 소문자로 바꾸는 메소드
+		
+		//	검색 관련 메서드
+		System.out.println(str.charAt(5));	//	5번 인덱스의 char
+		//	Java가 어느 인덱스에 있는가 (!중요!)
+		int pos = str.indexOf("Java");	//	문자열 내부에서 Java의 위치
+		System.out.println("1st Search Java:" + pos);	
+		pos = str.indexOf("Java", pos + 4);	//	뒤의 Java가 걸리게 하기 위해서 뒤로 밀어줌
+		System.out.println("2nd Search Java:" + pos);	 
+		
+		pos = str.indexOf("Java", pos + 4);
+		System.out.println("3rd Search Java:" + pos);	//	검색할 부분문자열이 없으면 -1을 반환
+		
+		pos = str.lastIndexOf("Java");	//	뒤에서 부터 검색
+		System.out.println("Last Index Java:" + pos);
+		
+		//	문자열 추출
+		System.out.println("SUBSTRING:" + str.substring(5));	//	5번열부터 끝까지
+		System.out.println("SUBSTRING:" + str.substring(5, 16));	//	5번부터 16번까지
+		
+		//	문자열 치환
+		System.out.println("REPLACE:" + str.replace("Programming", "Coding"));
+		
+		//	trimming: 화이트스페이스 제거(공백, tab, 개행 등)
+		String s6 = "                Hello                 ";
+		String s7 = ", Java"; 
+		
+		s6 = s6.trim();
+		System.out.println(s6+s7);
+		
+		//	문자열 분리: split()
+		String[] tokens = str.replace(",", "").split(" ");	//	공백을 기준으로 문자열 분리
+		System.out.println("SPLIT:" + Arrays.toString(tokens));
+		
+		//	문자열 비교: Unicode
+		//	compareTo (!매우 중요!), 데이터 내부의 대소비교
+		//	-> 0이면 순번이 길다, 값이 작으면 음수, 값이 크면 양수
+		System.out.println("ABC".compareTo("ABD"));	
+	}
+	
+	private static void stringBufferEx() {
+		//	String 클래스는 immutable
+		//	StringBuffer 클래스는 mutable
+		//	내부적으로 버퍼를 통해서 문자열을 만들고 최종적으로 String을 반환
+		
+		//	버퍼의 생성
+		StringBuffer sb = new StringBuffer("This");
+		
+		//	문자열 추가 : append()
+		sb.append(" is pencil");
+		
+		//	문자열 삽입 : insert()
+		sb.insert(7, " my");
+		
+		//	문자열 치환 : replace()
+		sb.replace(7, 10, " your");
+		System.out.println(sb);
+		
+		//	버퍼 길이 변경
+		sb.setLength(10);	//	10까지 남고 나머지는 날아감
+		System.out.println(sb);
+		
+		//	메서드 체이닝 기법 활용 가능
+		StringBuffer sb2 = new StringBuffer("This").append(" is pencil").insert(7, " my").replace(7, 10, " your");
+		String s = sb2.toString();	//	문자열로 변환
+		System.out.println(s);
 	}
 }
