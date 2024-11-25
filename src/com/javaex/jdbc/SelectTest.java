8b1097925e453e422637963d0fa1162ca24e823a
kimyk0101 <kimyk7131@gmail.com>
2024-11-25 11:15:21 +0900

    JEBC API

---
+++ src/com/javaex/jdbc/SelectTest.java
@@ -0,0 +1,53 @@
+package com.javaex.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectTest {
+	
+	//	프로토콜://위치:port 번호/스키마
+	static final String dburl = "jdbc:mysql://localhost:3306/hrdb";
+	static final String dbuser = "hrdb";		
+	static final String dbpass = "hrdb";
+	
+	//	departments 테이블로부터 {department_id} - {department_name}
+	public static void main(String[] args) {
+		Connection conn = null;		//	접속 정보 객체
+		Statement stmt = null;		//	쿼리 실행 객체
+		ResultSet rs = null;		//	결과 객체
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dburl, dbuser, dbpass);
+			//	접속 객체 출력
+//			System.out.println(conn);	
+//			System.out.println("연결 성공!");	
+			
+			stmt = conn.createStatement();	//	쿼리 수행 객체
+			String sql = "SELECT department_id, department_name " + "FROM departments";
+			
+			rs = stmt.executeQuery(sql);	//	-> Cursor
+			
+			while(rs.next()) {
+				int deptId = rs.getInt(1);	//	0이 아닌 1부터 시작
+				String deptName = rs.getString("department_name");
+			
+				System.out.printf("%d - %s%n", deptId, deptName);
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버를 찾지 못했습니다!");
+		} catch (SQLException e) {
+			System.err.println("SQLError!");
+		} finally {
+			try {
+				if (conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
