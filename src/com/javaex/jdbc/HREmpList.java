6ba744925904f776b6c8d68564278a563fcfa24c
kimyk0101 <kimyk7131@gmail.com>
2024-11-25 12:42:06 +0900

    JDBC practice 2

--- src/com/javaex/jdbc/HREmpList.java
+++ src/com/javaex/jdbc/HREmpList.java
@@ -4,6 +4,7 @@ package com.javaex.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class HREmpList {
@@ -20,6 +21,35 @@ public class HREmpList {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dburl, dbuser, dbpass);
+			
+			String sql = "SELECT " +
+					"e.first_name, e.last_name, m.first_name " +
+					"FROM employees e JOIN employees m " +
+					"ON e.manager_id = m.employee_id " +
+					"ORDER BY e.first_name";
+			
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				String firstName = rs.getString(1);
+				String lastName = rs.getString(2);
+				String managerName = rs.getString(3);
+				
+				System.out.printf("%s, %s, %s%n", firstName, lastName, managerName);
+			}
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			if (rs != null) rs.close();
+			if (stmt != null) stmt.close();
+			if (conn !=null) conn.close();
+		} catch (Exception e) {
+			
+		}
 	}
-
+	}
 }
