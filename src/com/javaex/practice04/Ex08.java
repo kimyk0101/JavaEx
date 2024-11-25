abcae43c5fb6ad2a9fd154b150f08b5b1bceb170
kimyk0101 <kimyk7131@gmail.com>
2024-11-25 18:10:05 +0900

    배열 8번 문제

--- src/com/javaex/practice04/Ex08.java
+++ src/com/javaex/practice04/Ex08.java
@@ -4,7 +4,7 @@ package com.javaex.practice04;
 	public static void main(String[] args) {
 		// 배열을 사용하여 1~45까지의 숫자 중 임의의 6개 숫자를 출력하세요.(중복체크)
 		int arr[] = new int[6];
-		
+
 		int prev = 0;
 		int now;
 		for (int i = 0; i < arr.length; i++) {
@@ -15,7 +15,7 @@ package com.javaex.practice04;
 					prev = now;
 					break;
 				}
-				
+
 			}
 			arr[i] = now;
 			System.out.print(arr[i] + "\t");
@@ -28,14 +28,16 @@ public class Ex08 {
 	public static void main(String[] args) {
 		// 배열을 사용하여 1~45까지의 숫자 중 임의의 6개 숫자를 출력하세요.(중복체크)
 		int arr[] = new int[6];
-
-		while (true) {
+		boolean run = true;
+		
+		while (run) {
 			for (int a = 0; a < arr.length; a++) {
 				if (arr[a] == arr[++a]) {
 					arr[++a] = (int)(Math.random() * 45) + 1;
 				}
 				System.out.print(arr[a] + "\t");
-			}				
+			}
+			run = false;
 		}	
 	}
 }
