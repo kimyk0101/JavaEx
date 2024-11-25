d113066fca3518c7aa371561c3184002c40e5ab7
kimyk0101 <kimyk7131@gmail.com>
2024-11-25 18:07:54 +0900

    배열 8번 문제

--- src/com/javaex/practice04/Ex08.java
+++ src/com/javaex/practice04/Ex08.java
@@ -1,51 +1,42 @@
 package com.javaex.practice04;
 
+/*public class Ex08 {
+	public static void main(String[] args) {
+		// 배열을 사용하여 1~45까지의 숫자 중 임의의 6개 숫자를 출력하세요.(중복체크)
+		int arr[] = new int[6];
+		
+		int prev = 0;
+		int now;
+		for (int i = 0; i < arr.length; i++) {
+
+			while (true) {
+				now = (int)(Math.random() * 45) + 1;		
+				if (now != prev) {
+					prev = now;
+					break;
+				}
+				
+			}
+			arr[i] = now;
+			System.out.print(arr[i] + "\t");
+		}	
+	}
+}*/
+
+
 public class Ex08 {
 	public static void main(String[] args) {
 		// 배열을 사용하여 1~45까지의 숫자 중 임의의 6개 숫자를 출력하세요.(중복체크)
 		int arr[] = new int[6];
-		int j =0;
-		
-		for (int i = 0; i < arr.length; i++) {
-			
-			while (true) {
-				arr[i] = (int)(Math.random() * 45) + 1;		
-				if (arr[i] != j) {
-					j = arr[i];
-					break;
+
+		while (true) {
+			for (int a = 0; a < arr.length; a++) {
+				if (arr[a] == arr[++a]) {
+					arr[++a] = (int)(Math.random() * 45) + 1;
 				}
-				System.out.print(arr[i] + "\t");
-				
-				
-			}
-		}
-
-
-		
+				System.out.print(arr[a] + "\t");
+			}				
+		}	
 	}
 }
 
-
-
-
-
-
-
-
-//int a[] = new int[50];
-//for(int i = 1; i <= 45; i++) {
-//	a[i] = i;
-//}
-//int prev = 0;
-//int now;
-//for (int i = 0; i < 6; i++) {
-//	
-//	while (true) {
-//		now = (int)(Math.random()*45) + 1;
-//		if (now != prev) {
-//			prev = now;
-//			break;
-//		}
-//	}
-//	System.out.printf(a[now] + "\t");
-//}
